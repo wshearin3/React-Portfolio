f130bddf3ef2d99218bbd270652c154a08f33339
Trey <trey.shearin3@gmail.com>
2024-06-03 10:59:25 -0400

    added imports for various components to app.js file

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
