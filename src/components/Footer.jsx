201d3adc16bde1e1c8a7134cb7298211eedd8637
Trey <trey.shearin3@gmail.com>
2024-06-03 18:08:39 -0400

    created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/footer.css';
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Connect with me:</p>
+      <a href="https://github.com/wshearin3" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/trey-shearin/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      
+    </footer>
+  );
+}
+
+export default Footer;
