b67a67d0bf51a312e25bff6f7c43ddeb992e1b77
Trey <trey.shearin3@gmail.com>
2024-06-03 11:00:54 -0400

    added function to app.js to switch between various components

--- src/App.js
+++ src/App.js
@@ -6,3 +6,22 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
+
+function App() {
+    const [currentSection, setCurrentSection] = useState('about');
+
+    const renderSection = () => {
+        switch (currentSection) {
+            case 'about':
+                return <AboutMe />;
+            case 'portfolio':
+                return <Portfolio />;
+            case 'contact':
+                return <Contact />;
+            case 'resume':
+                return <Resume />;
+            default:
+                return <AboutMe />;
+        }
+    };
+}
