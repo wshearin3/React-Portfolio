34c435ece81c54bd5f30704e21a71ff395f93405
Trey <trey.shearin3@gmail.com>
2024-06-03 13:14:03 -0400

    deleted files to restart application build

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Connect with me:</p>
+            <a href="https://github.com/wshearin3" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/trey-shearin/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </footer>
+    );
+}
+
+export default Footer;
